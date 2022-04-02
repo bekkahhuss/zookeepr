54e0319c160fa2226f7f5abf38e0aaa9ee70815d
bekkahhuss <bekkahhuss@gmail.com>
2022-04-02 13:46:53 -0400

    added get routes for html files

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 const { animals } = require("./data/animals.json");
 const { isAnyArrayBuffer } = require("util/types");
 
@@ -71,8 +72,11 @@ function createNewAnimal(body, animalsArray) {
 }
 
 function validateAnimal(animal) {
-    if (!animal.nam || typeof animal.name !== 'string') {
+    if (!animal.name || typeof animal.name !== 'string') {
         return false;  
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
     }
     if (!animal.diet || typeof animal.diet !== 'string') {
         return false;
@@ -100,17 +104,30 @@ app.get("/api/animals/:id", (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+ 
 app.post("/api/animals", (req, res) => {
   // set id based on what the next index of te array will be
   req.body.id = animals.length.toString();
 
   //if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
-      res.status(400).send('The animal is not properly formatted.');
-    } else {
-        const animal = createNewAnimal(req.body, animals);
-        res.json(animal);
-    }
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
 });
 
 app.listen(PORT, () => {
